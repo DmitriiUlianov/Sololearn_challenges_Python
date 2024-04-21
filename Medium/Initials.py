196c6c012b8ba9234f88886b7b72ce52a5557d96
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-04-21 17:37:12 +0200

    Create Initials.py

---
+++ Medium/Initials.py
@@ -0,0 +1,33 @@
+''' You are given a list of names for a fundraiser, but you need to keep the names relatively anonymous. You are tasked with getting the initials for each name provided. 
+ 
+Task:  
+Given a list of first and last names, take the first letter from each name to create initials and output them as a space-separated string. 
+ 
+Input Format:  
+The first input denotes the number of names in the list (N). The next N lines contain the list elements as strings.  
+ 
+Output Format:  
+A string containing the initials of each name in the original list, separated by spaces. 
+ 
+Sample Input:  
+3  
+Nick Dunburry 
+Tommy Newborne 
+David James 
+ 
+Sample Output:  
+ND TN DJ
+
+Explanation: 
+Taking the first letter from each name results in the output ND TN DJ. '''
+
+N = int(input())
+names = []
+for i in range(N):
+    names.append(input())
+
+num = 0
+while num < N:
+    print(names[num][0], end = "")
+    print(names[num][names[num].index(' ')+1], end = " ")
+    num += 1
